1159a84268c3766b2d50c669959bf9d37b18a6e1
Erin Jeter <ekjeter@gmail.com>
2021-05-12 20:33:15 -0400

    initial deploy

--- db.js
+++ db.js
@@ -1,7 +1,14 @@
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  host: "localhost",
   dialect: "postgres",
+  dialectOptions: !process.env.DATABASE_URL.includes("localhost")
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // <<<<<<< YOU NEED THIS TO FIX UNHANDLED REJECTION
+        },
+      }
+    : {},
 });
 
 sequelize.authenticate().then(
